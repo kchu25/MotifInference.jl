044786f61022d4ea0a8d09a754f917fb454d7277
Shane Kuei-Hsien Chu <skchu@wustl.edu>
2026-06-02 09:42:34 -0400

    update

---
+++ src/MotifInference.jl
@@ -0,0 +1,29 @@
+module MotifInference
+
+using CairoMakie
+using JLD2
+using CUDA
+
+const cnn2_path = joinpath(@__DIR__, "..", "VeryBasicCNN2", "VeryBasicCNN2.jl")
+include(cnn2_path)
+using .VeryBasicCNN2
+using AutoComputationalGraphTuning
+using Flux
+using SEQ2EXPdata
+using GlyphEctoplasm
+using BanzhafInference
+using StatsBase
+using Arrow
+
+include(joinpath(@__DIR__, "..", "struct_def.jl"))
+include(joinpath(@__DIR__, "..", "tuning_and_train_final.jl"))
+include(joinpath(@__DIR__, "..", "plotting.jl"))
+include(joinpath(@__DIR__, "..", "run_thru.jl"))
+
+include(joinpath(@__DIR__, "..", "dataset_utils.jl"))
+include(joinpath(@__DIR__, "..", "datasets.jl"))
+include(joinpath(@__DIR__, "..", "pipeline.jl"))
+
+export VeryBasicCNN2, DATASETS, DATASETS_MUT, DATASETS_DEBUG
+
+end
