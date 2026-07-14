f4381b6a90d3c0c96015dde1c5297cb07d6d42b5
shane <skchu@wustl.edu>
2026-07-14 13:19:22 -0400

    update BOTTLENECK_HEIGHT

--- src/VeryBasicCNN2/VeryBasicCNN2.jl
+++ src/VeryBasicCNN2/VeryBasicCNN2.jl
@@ -15,7 +15,7 @@ using Statistics: mean, var
 const DEFAULT_FLOAT_TYPE = Float32
 const SOFTMAX_ALPHA = DEFAULT_FLOAT_TYPE(500)
 const BOTTLENECK_FILTERS = 50
-const BOTTLENECK_HEIGHT = 3
+const BOTTLENECK_HEIGHT = 8
 # ────────────────────────────────────────────────────────────────────────────
 # Core Components (Order Matters)
 # ────────────────────────────────────────────────────────────────────────────
